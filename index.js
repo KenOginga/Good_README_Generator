c369beb582f523562f7b1f0b134e75ef97096076
KenOginga <72943649+KenOginga@users.noreply.github.com>
2020-11-25 17:00:39 -0600

    modified the files and made some final changes.

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
-// array of questions for user
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 const questions = [
+    {
+        name: "githubUsername",
+        message: "What is your GITHUB username?"
+    },
+    {
+        name: "emailAddress",
+        message: "What is your email address?"
+    },
+    {
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        name: "projectDescription",
+        message: "Write a short description of your project"
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "What kind of licence will your project have?",
+        choices: [
+            "MIT",
+            "MPL",
+            "GPL",
+            "AGPL-3.0"
+        ]
+    },
+    {
+        name: "dependecies",
+        message: "What commands should be run to install the dependecies?"
+    },
+    {
+        name: "contributors",
+        message: "Name the contributor(s) in this project."
+    }
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    console.log(data)
+    fs.writeFile("README.md", generateMarkdown(data), (err) =>
+    err ? console.log(err)
+        : console.log("Generated a readme.md file.")
+        );
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then(writeToFile)  
 }
-
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}`;
+    return `
+    # **${data.title}**
+    
+       
+    # Project Description
+        ${data.projectDescription}
+
+
+    # Table of Contents
+      * [Project Description](#projectDescription)
+
+      * [Installation](#dependecies)
+
+      * [Licence](#license)
+      
+    # Installation
+        ${data.dependencies}
+
+
+    # License
+        ${data.license}
+
+
+    # Contributor(s)
+        ${data.contributors}
+
+    # Demo/Screenshot
+    
+
+    # Questions
+    In case you have any questions, contact the project owner using the contacts below:
+
+    #### Email address: ${data.emailAddress}
+    #### GitHub Account: ${data.githubUsername}
+
+    `;
     
   }
   
